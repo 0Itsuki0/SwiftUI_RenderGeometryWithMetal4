a443a0884536d69aed3ee7f008153f6a0d046b93
itsuki <itsuki.enjyoy@gmail.com>
2025-09-15 08:07:46 +0900

    Initial Commit

---
+++ ViewWithMetal/ViewWithMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewWithMetalApp.swift
+//  ViewWithMetal
+//
+//  Created by Itsuki on 2025/09/15.
+//
+
+import SwiftUI
+
+@main
+struct ViewWithMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
